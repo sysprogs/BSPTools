80a83803b5923255cc85d0d0bfbb3b7477e43f5f
Sysprogs <sysprogs@sysprogs.com>
2016-04-21 13:20:43 -0700

    Added support for generating ESP8266 binary images without esptool.py

---
+++ DebugPackages/ESP8266DebugPackage/ESP8266BinaryImage.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace ESP8266DebugPackage
+{
+    public class ArgumentValueAttribute : Attribute
+    {
+        public readonly string Name;
+
+        public ArgumentValueAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class ESP8266BinaryImage
+    {
+        public struct Segment
+        {
+            public uint Address;
+            public byte[] Data;
+        }
+
+        public enum FLASHMode
+        {
+            [ArgumentValue("qio")]
+            QIO = 0,
+            [ArgumentValue("qout")]
+            QOUT,
+            [ArgumentValue("dio")]
+            DIO,
+            [ArgumentValue("dout")]
+            DOUT
+        }
+
+        public enum FLASHSize
+        {
+            [ArgumentValue("4m")]
+            size4M = 0,
+            [ArgumentValue("2m")]
+            size2M,
+            [ArgumentValue("8m")]
+            size8M,
+            [ArgumentValue("16m")]
+            size16M,
+            [ArgumentValue("32m")]
+            size32M,
+            [ArgumentValue("16m-c1")]
+            size16M_c1,
+            [ArgumentValue("32m-c1")]
+            size32M_c1,
+            [ArgumentValue("32m-c2")]
+            size32M_c2,
+        }
+
+        public enum FLASHFrequency
+        {
+            [ArgumentValue("40m")]
+            freq40M = 0,
+            [ArgumentValue("26m")]
+            freq26M,
+            [ArgumentValue("20m")]
+            freq20M,
+            [ArgumentValue("80m")]
+            freq80M = 0x0f
+        }
+
+        static _Ty ParseEnumValue<_Ty>(string arg)
+        {
+            foreach(FieldInfo fld in typeof(_Ty).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var attr = fld.GetCustomAttributes(typeof(ArgumentValueAttribute), false);
+                if (attr != null && attr.Length > 0)
+                {
+                    if ((attr[0] as ArgumentValueAttribute).Name == arg)
+                        return (_Ty)fld.GetValue(null);
+                }
+            }
+            return default(_Ty);
+        }
+
+        public struct ParsedHeader
+        {
+            public FLASHSize Size;
+            public FLASHFrequency Frequency;
+            public FLASHMode Mode;
+
+            public ParsedHeader(string frequency, string mode, string size)
+            {
+                Size = ParseEnumValue<FLASHSize>(size);
+                Frequency = ParseEnumValue<FLASHFrequency>(frequency);
+                Mode = ParseEnumValue<FLASHMode>(mode);
+            }
+        }
+
+        static void UpdateChecksum(ref byte checksum, byte[] data)
+        {
+            foreach (var b in data)
+                checksum ^= b;
+        }
+
+        public ParsedHeader Header;
+        public List<Segment> Segments = new List<Segment>();
+        public uint EntryPoint;
+
+        public void Save(Stream stream)
+        {
+            byte checksum = 0xef;
+            long offBase = stream.Position;
+            stream.WriteByte(0xe9);
+            stream.WriteByte((byte)Segments.Count);
+            stream.WriteByte((byte)Header.Mode);
+            stream.WriteByte((byte)(((byte)Header.Size << 4) | (byte)Header.Frequency));
+            stream.Write(BitConverter.GetBytes(EntryPoint), 0, 4);
+            foreach(var segment in Segments)
+            {
+                stream.Write(BitConverter.GetBytes(segment.Address), 0, 4);
+                stream.Write(BitConverter.GetBytes(segment.Data.Length), 0, 4);
+                stream.Write(segment.Data, 0, segment.Data.Length);
+                UpdateChecksum(ref checksum, segment.Data);
+            }
+
+            int alignment = (int)(15 - ((stream.Position - offBase) % 16));
+            stream.Write(new byte[alignment], 0, alignment);
+            stream.WriteByte(checksum);
+        }
+
+        const uint SPIFLASHBase = 0x40200000;
+        const uint SPIFLASHLimit = 0x40300000;
+
+        public static ESP8266BinaryImage BaseImageFromELFFile(ELFFile file, ParsedHeader header, bool esptoolSectionOrder = false)
+        {
+            ESP8266BinaryImage image = new ESP8266BinaryImage();
+            image.EntryPoint = file.ELFHeader.e_entry;
+            image.Header = header;
+
+            List<ELFFile.ParsedSection> sections = new List<ELFFile.ParsedSection>();
+            if (esptoolSectionOrder)
+            {
+                foreach (var secN in new string[] { ".text", ".data", ".rodata" })
+                    sections.Add(file.FindSectionByName(secN));
+            }
+            else
+                sections = file.AllSections;
+
+            foreach (var sec in sections)
+            {
+                if (sec.HasData && sec.PresentInMemory && sec.VirtualAddress < SPIFLASHBase)
+                {
+                    var segment = new Segment { Address = sec.VirtualAddress, Data = file.LoadSection(sec) };
+                    int align = ((segment.Data.Length + 3) & ~3) - segment.Data.Length;
+                    if (align > 0)
+                        Array.Resize(ref segment.Data, segment.Data.Length + align);
+                    image.Segments.Add(segment);
+                }
+            }
+            return image;
+        }
+
+        public struct SPIFLASHSection
+        {
+            public ulong OffsetInFLASH;
+            public byte[] Data;
+        }
+
+        public static SPIFLASHSection[] GetFLASHSections(ELFFile file)
+        {
+            List<SPIFLASHSection> sections = new List<SPIFLASHSection>();
+            foreach (var sec in file.AllSections)
+            {
+                if (sec.VirtualAddress >= SPIFLASHBase && sec.VirtualAddress < SPIFLASHLimit)
+                    sections.Add(new SPIFLASHSection { OffsetInFLASH = sec.VirtualAddress - SPIFLASHBase, Data = file.LoadSection(sec) });
+            }
+            return sections.ToArray();
+        }
+    }
+}
--- DebugPackages/ESP8266DebugPackage/ESP8266StartupSequence.cs
+++ DebugPackages/ESP8266DebugPackage/ESP8266StartupSequence.cs
@@ -201,63 +201,40 @@ namespace ESP8266DebugPackage
             string bspPath = bspDict["SYS:BSP_ROOT"];
             string toolchainPath = bspDict["SYS:TOOLCHAIN_ROOT"];
 
-            string pythonPath = Path.GetDirectoryName(bspPath) + @"\python27\python.exe";
-            if (!File.Exists(pythonPath))
-                throw new Exception("Python not found: " + pythonPath);
-
-            string esptoolPath = bspPath + @"\esptool.py";
-            if (!File.Exists(esptoolPath))
-                throw new Exception("Esptool not found: " + esptoolPath);
-
             Regex rgBinFile = new Regex("^" + Path.GetFileName(targetPath) + "-0x([0-9a-fA-F]+)\\.bin$", RegexOptions.IgnoreCase);
             foreach (var fn in Directory.GetFiles(Path.GetDirectoryName(targetPath)))
                 if (rgBinFile.IsMatch(Path.GetFileName(fn)))
                     File.Delete(fn);
 
-            string val;
-            string args = string.Format("\"{0}\" elf2image \"{1}\"", esptoolPath, targetPath);
-            if (debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_freq", out val) && !string.IsNullOrEmpty(val))
-                args += " --flash_freq " + val;
-            if (debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_mode", out val) && !string.IsNullOrEmpty(val))
-                args += " --flash_mode " + val;
-            if (debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_size", out val) && !string.IsNullOrEmpty(val))
-                args += " --flash_size " + val;
-
-            if (lineHandler != null)
-                lineHandler(pythonPath + " " + args, false);
-
-            var startInfo = new ProcessStartInfo { FileName = pythonPath, Arguments = args, CreateNoWindow = true, UseShellExecute = false, RedirectStandardOutput = true, RedirectStandardError = true };
-            startInfo.EnvironmentVariables["PATH"] += ";" + toolchainPath + @"\bin";
-
-            var proc = Process.Start(startInfo);
-            if (lineHandler != null)
-            {
-                proc.OutputDataReceived += (p, e) => lineHandler(e.Data, true);
-                proc.ErrorDataReceived += (p, e) => lineHandler(e.Data, true);
-            }
-            proc.BeginOutputReadLine();
-            proc.BeginErrorReadLine();
-
-            if (!proc.WaitForExit(30000))
-            {
-                proc.Kill();
-                throw new Exception("ESPTool appears to be hanging");
-            }
-
-            if (proc.ExitCode != 0)
-                throw new Exception("ESPTool returned an error " + proc.ExitCode);
+            string freq, mode, size;
+            debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_freq", out freq);
+            debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_mode", out mode);
+            debugMethodConfig.TryGetValue("com.sysprogs.esp8266.xt-ocd.flash_size", out size);
 
             List<ProgrammableRegion> regions = new List<ProgrammableRegion>();
 
-            foreach (var fn in Directory.GetFiles(Path.GetDirectoryName(targetPath)))
+            using (var elfFile = new ELFFile(targetPath))
             {
-                var m = rgBinFile.Match(Path.GetFileName(fn));
-                if (m.Success)
-                    regions.Add(new ProgrammableRegion { FileName = fn, Offset = int.Parse(m.Groups[1].Value, System.Globalization.NumberStyles.HexNumber), Size = File.ReadAllBytes(fn).Length });
-            }
+                string pathBase = Path.Combine(Path.GetDirectoryName(targetPath), Path.GetFileName(targetPath));
+                var img = ESP8266BinaryImage.BaseImageFromELFFile(elfFile, new ESP8266BinaryImage.ParsedHeader(freq, mode, size));
+                string fn = pathBase + "-0x00000.bin";
+                using (var fs = new FileStream(fn, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    img.Save(fs);
+                    regions.Add(new ProgrammableRegion { FileName = fn, Offset = 0, Size = (int)fs.Length });
+                }
 
-            if (regions.Count == 0)
-                throw new Exception("ESPTool did not produce any binary files");
+
+                foreach (var sec in ESP8266BinaryImage.GetFLASHSections(elfFile))
+                {
+                    fn = string.Format("{0}-0x{1:x5}.bin", pathBase, sec.OffsetInFLASH);
+                    using (var fs = new FileStream(fn, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    {
+                        fs.Write(sec.Data, 0, sec.Data.Length);
+                        regions.Add(new ProgrammableRegion { FileName = fn, Offset = (int)sec.OffsetInFLASH, Size = sec.Data.Length });
+                    }
+                }
+            }
             return regions;
         }
 
