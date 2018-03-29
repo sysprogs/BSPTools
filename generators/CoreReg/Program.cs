08c1cf85e26d3ffa3ddc3902754a4defbc5fa5e1
Sysprogs <sysprogs@sysprogs.com>
2018-03-29 12:10:15 -0700

    Recovered missing NVIC registers for Cortex-M3 and higher

--- generators/CoreReg/Program.cs
+++ generators/CoreReg/Program.cs
@@ -14,9 +14,59 @@ namespace CoreReg
 {
     class Program
     {
-
-        static public void SaveReg(HardwareRegisterSet registers, string pOutFolder, CortexCore core)
+        public static UInt64? TryParseMaybeHex(string str)
         {
+            if (str == null)
+                return null;
+
+            int idx = str.IndexOf(' ');
+            if (idx != -1)
+                str = str.Substring(0, idx);
+
+            ulong addr;
+
+            if (str.StartsWith("0x"))
+                return UInt64.TryParse(str.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier, null, out addr) ? new ulong?(addr) : null;
+            else
+                return UInt64.TryParse(str, out addr) ? new ulong?(addr) : null;
+        }
+
+        static public void SaveReg(HardwareRegisterSet registers, string pOutFolder, CortexCore core, bool extendNVICRegisters)
+        {
+            if (extendNVICRegisters)
+            {
+                var lst = registers.Registers.ToList();
+                Regex rgName = new Regex("NVIC_(.*)3");
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    var m = rgName.Match(lst[i].Name);
+                    if (m.Success)
+                    {
+                        if (lst[i - 1].Name != lst[i].Name.Replace("3", "2"))
+                            continue;
+
+                        if (lst[i + 1].Name == lst[i].Name.Replace("3", "4"))
+                            continue;
+                        else
+                        {
+                            var originalReg = lst[i];
+                            ulong address = TryParseMaybeHex(originalReg.Address).Value;
+                            ulong increment = TryParseMaybeHex(lst[i].Address).Value - TryParseMaybeHex(lst[i - 1].Address).Value;
+                            for (int j = 4; j < 8; j++)
+                            {
+                                var reg = XmlTools.LoadObjectFromString<HardwareRegister>(XmlTools.SaveObjectToString(originalReg));
+                                reg.Name = $"NVIC_{m.Groups[1].Value}{j}";
+                                address += increment;
+                                reg.Address = $"0x{address:x8}";
+                                lst.Insert(++i, reg);
+                            }
+                        }
+                    }
+                }
+
+                registers.Registers = lst.ToArray();
+            }
+
             registers.UserFriendlyName = "ARM Cortex " + (core.ToString().Replace("Plus", "+"));
             string outputFile = $@"{pOutFolder}\core_{core}.xml";
             Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
@@ -50,7 +100,7 @@ namespace CoreReg
             string aDirCoreReg = @"..\..\OutCorexx"; //bspDir.OutputDir
 
             var setsCortexM0 = SVDParser.ParseSVDFileToHardSet(Path.Combine(bspDir.RulesDir, @"XMC1100.svd"), "Cortex-M0 Private Peripheral Block");
-            SaveReg(setsCortexM0, aDirCoreReg, CortexCore.M0);
+            SaveReg(setsCortexM0, aDirCoreReg, CortexCore.M0, false);
             if (setsCortexM0 == null)
                 throw new Exception("No ppb reg M0");
 
@@ -59,7 +109,7 @@ namespace CoreReg
             if (setsCortexM4 == null)
                 throw new Exception("No ppb reg M4");
 
-            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M4);
+            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M4, true);
 
             // Cortex M3---
             registers = new List<HardwareRegister>(setsCortexM4.Registers);
@@ -72,7 +122,7 @@ namespace CoreReg
             registers.Remove(registers.FirstOrDefault(f => "FPDSCR" == f.Name));
             setsCortexM4.Registers = registers.ToArray();
 
-            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M3);
+            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M3, true);
 
             // Cortex M0Plus---
             registers.Clear();
@@ -80,7 +130,7 @@ namespace CoreReg
             registers.AddRange(LoadAddReg(Path.Combine(bspDir.RulesDir, "core_m0mpu.txt")));
             setsCortexM4.Registers = registers.ToArray();
 
-            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M0Plus);
+            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M0Plus, false);
 
             // Cortex M7---
             setsCortexM4 = SVDParser.ParseSVDFileToHardSet(Path.Combine(bspDir.InputDir, @"CMSIS\Infineon\SVD\XMC4100.svd"), "Cortex-M4 Private Peripheral Block");
@@ -89,7 +139,7 @@ namespace CoreReg
             registers.AddRange(LoadAddReg(Path.Combine(bspDir.RulesDir, "core_m7ppb.txt")));
             setsCortexM4.Registers = registers.ToArray();
 
-            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M7);
+            SaveReg(setsCortexM4, aDirCoreReg, CortexCore.M7, true);
         }
 
     }
