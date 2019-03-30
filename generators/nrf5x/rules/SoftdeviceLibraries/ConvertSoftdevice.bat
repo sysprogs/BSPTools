f57480bb9899e8508d7d790822a9a6f5957d7f20
Sysprogs <sysprogs@sysprogs.com>
2019-03-29 21:20:07 -0700

    Gaps in the softdevice HEX file are now filled with 0xFF as the softdevice expects

--- generators/nrf5x/rules/SoftdeviceLibraries/ConvertSoftdevice.bat
+++ generators/nrf5x/rules/SoftdeviceLibraries/ConvertSoftdevice.bat
@@ -2,5 +2,5 @@ set ARM_GCC_PREFIX=arm-eabi-
 cd /d %~dp0
 
 %ARM_GCC_PREFIX%gcc empty.c -c -o %1_softdevice.o -mcpu=cortex-m4 -mthumb %3
-%ARM_GCC_PREFIX%objcopy.exe -I ihex -O binary %2 %1_softdevice.bin
+%ARM_GCC_PREFIX%objcopy.exe --gap-fill 0xFF -I ihex -O binary %2 %1_softdevice.bin
 %ARM_GCC_PREFIX%objcopy.exe --add-section .softdevice=%1_softdevice.bin --set-section-flags .softdevice=CONTENTS,ALLOC,LOAD,READONLY,CODE %1_softdevice.o --remove-section .text --remove-section .data --remove-section .bss
