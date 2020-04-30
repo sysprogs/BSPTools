a071e09bbaf99954b155ebd1ad3fcd267136cf5e
Sysprogs <sysprogs@sysprogs.com>
2020-04-29 18:36:28 -0700

    Updated Tiva BSP generator

--- generators/TI/Tiva/Program.cs
+++ generators/TI/Tiva/Program.cs
@@ -12,6 +12,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Tiva_bsp_generator
@@ -34,7 +35,7 @@ namespace Tiva_bsp_generator
                 ramBase = SRAMBase;
             }
 
-            public override MemoryLayout GetMemoryLayout(MCUBuilder mcu, MCUFamilyBuilder family)
+            public override MemoryLayoutAndSubstitutionRules GetMemoryLayout(MCUBuilder mcu, MCUFamilyBuilder family)
             {
                 //No additional memory information available for this MCU. Build a basic memory layout from known RAM/FLASH sizes.
                 MemoryLayout layout = new MemoryLayout { DeviceName = mcu.Name, Memories = new List<Memory>() };
@@ -56,7 +57,7 @@ namespace Tiva_bsp_generator
                     Size = (uint)mcu.RAMSize,
                 });
 
-                return layout;
+                return new MemoryLayoutAndSubstitutionRules(layout);
             }
         }
 
@@ -142,107 +143,124 @@ namespace Tiva_bsp_generator
             if (args.Length < 1)
                 throw new Exception("Usage: tiva.exe <Tiva SW package directory>");
 
-            var bspBuilder = new TivaBSPBuilder(new BSPDirectories(args[0], @"..\..\Output", @"..\..\rules"));
-
-            var devices = BSPGeneratorTools.ReadMCUDevicesFromCommaDelimitedCSVFile(bspBuilder.Directories.RulesDir + @"\Tivadevices.csv",
-                "Part Number", "Flash (KB)", "SRAM(kB)", "CPU", true);
-
-            List<MCUFamilyBuilder> allFamilies = new List<MCUFamilyBuilder>();
-            foreach (var fn in Directory.GetFiles(bspBuilder.Directories.RulesDir + @"\Families", "*.xml"))
-                allFamilies.Add(new MCUFamilyBuilder(bspBuilder, XmlTools.LoadObject<FamilyDefinition>(fn)));
-
-            var rejects = BSPGeneratorTools.AssignMCUsToFamilies(devices, allFamilies);
-            List<MCUFamily> familyDefinitions = new List<MCUFamily>();
-            List<MCU> mcuDefinitions = new List<MCU>();
-            List<EmbeddedFramework> frameworks = new List<EmbeddedFramework>();
-            List<MCUFamilyBuilder.CopiedSample> exampleDirs = new List<MCUFamilyBuilder.CopiedSample>();
-
-            bool noPeripheralRegisters = args.Contains("/noperiph");
-            List<KeyValuePair<string, string>> macroToHeaderMap = new List<KeyValuePair<string, string>>();
-
-            var commonPseudofamily = new MCUFamilyBuilder(bspBuilder, XmlTools.LoadObject<FamilyDefinition>(bspBuilder.Directories.RulesDir + @"\CommonFiles.xml"));
-            var flags = new ToolFlags();
-            List<string> projectFiles = new List<string>();
-            commonPseudofamily.CopyFamilyFiles(ref flags, projectFiles);
-
-            foreach (var sample in commonPseudofamily.CopySamples())
-                exampleDirs.Add(sample);
-
-            foreach (var fam in allFamilies)
+            using (var bspBuilder = new TivaBSPBuilder(new BSPDirectories(args[0], @"..\..\Output", @"..\..\rules", @"..\..\log")))
             {
-                var rejectedMCUs = fam.RemoveUnsupportedMCUs(true);
-                if (rejectedMCUs.Length != 0)
+                var devices = BSPGeneratorTools.ReadMCUDevicesFromCommaDelimitedCSVFile(bspBuilder.Directories.RulesDir + @"\Tivadevices.csv",
+                    "Part Number", "Flash (KB)", "SRAM(kB)", "CPU", true);
+
+                HashSet<string> foundDevices = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+                foreach (var dev in devices)
+                    foundDevices.Add(dev.Name);
+
+                foreach(var fn in Directory.GetFiles(Path.Combine(bspBuilder.Directories.InputDir, "inc"), "tm4c*.h"))
                 {
-                    Console.WriteLine("Unsupported {0} MCUs:", fam.Definition.Name);
-                    foreach (var mcu in rejectedMCUs)
-                        Console.WriteLine("\t{0}", mcu.Name);
+                    if (!foundDevices.Contains(Path.GetFileNameWithoutExtension(fn)))
+                    {
+                        throw new Exception("Device from SDK not mentioned in CSV: " + fn);
+                    }
                 }
 
-                foreach(var mcu in fam.MCUs)
-                {
-                    string fn = string.Format("{0}\\inc\\{1}.h", fam.Definition.PrimaryHeaderDir, mcu.Name);
-                    if (!File.Exists(fn))
-                        throw new Exception("Missing device header file");
-                    macroToHeaderMap.Add(new KeyValuePair<string, string>(mcu.Name, mcu.Name.ToLower() + ".h"));
-                }
+                List<MCUFamilyBuilder> allFamilies = new List<MCUFamilyBuilder>();
+                foreach (var fn in Directory.GetFiles(bspBuilder.Directories.RulesDir + @"\Families", "*.xml"))
+                    allFamilies.Add(new MCUFamilyBuilder(bspBuilder, XmlTools.LoadObject<FamilyDefinition>(fn)));
 
-                fam.AttachStartupFiles(ParseStartupFiles(fam.Definition.PrimaryHeaderDir, "startup_gcc.c", fam));
-                if (!noPeripheralRegisters)
-                    fam.AttachPeripheralRegisters(ParsePeripheralRegisters(fam.Definition.PrimaryHeaderDir));
+                var rejects = BSPGeneratorTools.AssignMCUsToFamilies(devices, allFamilies);
+                List<MCUFamily> familyDefinitions = new List<MCUFamily>();
+                List<MCU> mcuDefinitions = new List<MCU>();
+                List<EmbeddedFramework> frameworks = new List<EmbeddedFramework>();
+                List<MCUFamilyBuilder.CopiedSample> exampleDirs = new List<MCUFamilyBuilder.CopiedSample>();
 
-                var famObj = fam.GenerateFamilyObject(true);
+                bool noPeripheralRegisters = args.Contains("/noperiph");
+                List<KeyValuePair<string, string>> macroToHeaderMap = new List<KeyValuePair<string, string>>();
 
-                famObj.AdditionalSourceFiles = LoadedBSP.Combine(famObj.AdditionalSourceFiles, projectFiles.Where(f => !MCUFamilyBuilder.IsHeaderFile(f)).ToArray());
-                famObj.AdditionalHeaderFiles = LoadedBSP.Combine(famObj.AdditionalHeaderFiles, projectFiles.Where(f => MCUFamilyBuilder.IsHeaderFile(f)).ToArray());
+                var commonPseudofamily = new MCUFamilyBuilder(bspBuilder, XmlTools.LoadObject<FamilyDefinition>(bspBuilder.Directories.RulesDir + @"\CommonFiles.xml"));
+                var flags = new ToolFlags();
+                List<string> projectFiles = new List<string>();
+                commonPseudofamily.CopyFamilyFiles(ref flags, projectFiles);
 
-                famObj.AdditionalSystemVars = LoadedBSP.Combine(famObj.AdditionalSystemVars, commonPseudofamily.Definition.AdditionalSystemVars);
-                famObj.CompilationFlags = famObj.CompilationFlags.Merge(flags);
-
-                familyDefinitions.Add(famObj);
-                fam.GenerateLinkerScripts(false);
-                foreach (var mcu in fam.MCUs)
-                    mcuDefinitions.Add(mcu.GenerateDefinition(fam, bspBuilder, !noPeripheralRegisters));
-
-                foreach (var fw in fam.GenerateFrameworkDefinitions())
-                    frameworks.Add(fw);
-
-                foreach (var sample in fam.CopySamples())
+                foreach (var sample in commonPseudofamily.CopySamples())
                     exampleDirs.Add(sample);
-            }
 
-            using (var sw = File.CreateText(Path.Combine(bspBuilder.BSPRoot, "SDK", "inc", "tiva_device.h")))
-            {
-                sw.WriteLine("#pragma once");
-                sw.WriteLine();
-                bool first = true;
-                foreach(var kv in macroToHeaderMap)
+                foreach (var fam in allFamilies)
                 {
-                    sw.WriteLine("#{0}if defined({1})", first ? "" : "el", kv.Key);
-                    sw.WriteLine("\t#include \"{0}\"", kv.Value);
-                    first = false;
+                    var rejectedMCUs = fam.RemoveUnsupportedMCUs();
+                    if (rejectedMCUs.Length != 0)
+                    {
+                        Console.WriteLine("Unsupported {0} MCUs:", fam.Definition.Name);
+                        foreach (var mcu in rejectedMCUs)
+                            Console.WriteLine("\t{0}", mcu.Name);
+                    }
+
+                    foreach (var mcu in fam.MCUs)
+                    {
+                        string fn = string.Format("{0}\\inc\\{1}.h", fam.Definition.PrimaryHeaderDir, mcu.Name);
+                        if (!File.Exists(fn))
+                            throw new Exception("Missing device header file");
+                        macroToHeaderMap.Add(new KeyValuePair<string, string>(mcu.Name, mcu.Name.ToLower() + ".h"));
+                    }
+
+                    fam.AttachStartupFiles(ParseStartupFiles(fam.Definition.PrimaryHeaderDir, "startup_gcc.c", fam));
+                    if (!noPeripheralRegisters)
+                        fam.AttachPeripheralRegisters(ParsePeripheralRegisters(fam.Definition.PrimaryHeaderDir));
+
+                    var famObj = fam.GenerateFamilyObject(true);
+
+                    famObj.AdditionalSourceFiles = LoadedBSP.Combine(famObj.AdditionalSourceFiles, projectFiles.Where(f => !MCUFamilyBuilder.IsHeaderFile(f)).ToArray());
+                    famObj.AdditionalHeaderFiles = LoadedBSP.Combine(famObj.AdditionalHeaderFiles, projectFiles.Where(f => MCUFamilyBuilder.IsHeaderFile(f)).ToArray());
+
+                    famObj.AdditionalSystemVars = LoadedBSP.Combine(famObj.AdditionalSystemVars, commonPseudofamily.Definition.AdditionalSystemVars);
+                    famObj.CompilationFlags = famObj.CompilationFlags.Merge(flags);
+
+                    familyDefinitions.Add(famObj);
+                    fam.GenerateLinkerScripts(false);
+                    foreach (var mcu in fam.MCUs)
+                        mcuDefinitions.Add(mcu.GenerateDefinition(fam, bspBuilder, !noPeripheralRegisters));
+
+                    foreach (var fw in fam.GenerateFrameworkDefinitions())
+                        frameworks.Add(fw);
+
+                    foreach (var sample in fam.CopySamples())
+                        exampleDirs.Add(sample);
                 }
 
-                sw.WriteLine("#else");
-                sw.WriteLine("#error Device type not specified");
-                sw.WriteLine("#endif");
+                using (var sw = File.CreateText(Path.Combine(bspBuilder.BSPRoot, "SDK", "inc", "tiva_device.h")))
+                {
+                    sw.WriteLine("#pragma once");
+                    sw.WriteLine();
+                    bool first = true;
+                    foreach (var kv in macroToHeaderMap)
+                    {
+                        sw.WriteLine("#{0}if defined({1})", first ? "" : "el", kv.Key);
+                        sw.WriteLine("\t#include \"{0}\"", kv.Value);
+                        first = false;
+                    }
+
+                    sw.WriteLine("#else");
+                    sw.WriteLine("#error Device type not specified");
+                    sw.WriteLine("#endif");
+                }
+
+                Regex rgVersion = new Regex("\"version\" : \"([0-9.]+)\",");
+                var version = File.ReadAllLines(Path.Combine(bspBuilder.Directories.InputDir, "package.tirex.json")).Select(l => rgVersion.Match(l)).FirstOrDefault(m => m.Success)?.Groups[1].Value 
+                    ?? throw new Exception("Could not extract package version");
+
+                BoardSupportPackage bsp = new BoardSupportPackage
+                {
+                    PackageID = "com.sysprogs.arm.ti.tiva",
+                    PackageDescription = "TI Tiva Devices",
+                    GNUTargetID = "arm-eabi",
+                    GeneratedMakFileName = "tiva.mak",
+                    MCUFamilies = familyDefinitions.ToArray(),
+                    SupportedMCUs = mcuDefinitions.ToArray(),
+                    Frameworks = frameworks.ToArray(),
+                    Examples = exampleDirs.Where(s => !s.IsTestProjectSample).Select(s => s.RelativePath).ToArray(),
+                    TestExamples = exampleDirs.Where(s => s.IsTestProjectSample).Select(s => s.RelativePath).ToArray(),
+
+                    PackageVersion = version,
+                };
+
+                bspBuilder.Save(bsp, true);
             }
-
-            BoardSupportPackage bsp = new BoardSupportPackage
-            {
-                PackageID = "com.sysprogs.arm.ti.tiva",
-                PackageDescription = "TI Tiva Devices",
-                GNUTargetID = "arm-eabi",
-                GeneratedMakFileName = "tiva.mak",
-                MCUFamilies = familyDefinitions.ToArray(),
-                SupportedMCUs = mcuDefinitions.ToArray(),
-                Frameworks = frameworks.ToArray(),
-                Examples = exampleDirs.Where(s => !s.IsTestProjectSample).Select(s => s.RelativePath).ToArray(),
-                TestExamples = exampleDirs.Where(s => s.IsTestProjectSample).Select(s => s.RelativePath).ToArray(),
-
-                PackageVersion = "2.1.4.178"
-            };
-
-            bspBuilder.Save(bsp, true);
         }
     }
 }
