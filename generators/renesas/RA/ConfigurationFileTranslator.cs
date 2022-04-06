11cb870b3b01ccac44acf2275bbd392d32c3795d
Sysprogs <sysprogs@sysprogs.com>
2022-04-06 16:00:03 -0700

    Fixed generation of pinconfig files

--- generators/renesas/RA/ConfigurationFileTranslator.cs
+++ generators/renesas/RA/ConfigurationFileTranslator.cs
@@ -41,62 +41,65 @@ namespace renesas_ra_bsp_generator
                 string baseIndent = null;
                 List<GeneratedConfigurationFile.Fragment> fragments = new List<GeneratedConfigurationFile.Fragment>();
 
-                foreach (var rawLine in cf.SelectSingleNode("content").InnerText.Split('\n'))
+                if (!TranslateSpecialConfigFile(cf, fragments))
                 {
-                    var line = rawLine.TrimEnd();
-                    if (line.Trim() == "" && configLines.Count == 0)
-                        continue;
-
-                    if (baseIndent == null)
-                        baseIndent = line.Substring(0, line.Length - line.TrimStart(' ', '\t').Length);
-
-                    if (line.StartsWith(baseIndent))
-                        line = line.Substring(baseIndent.Length);
-
-                    if (line.StartsWith("#include"))
+                    foreach (var rawLine in cf.SelectSingleNode("content").InnerText.Split('\n'))
                     {
-                        var m = rgIncludeRA.Match(line);
-                        if (m.Success)
+                        var line = rawLine.TrimEnd();
+                        if (line.Trim() == "" && configLines.Count == 0)
+                            continue;
+
+                        if (baseIndent == null)
+                            baseIndent = line.Substring(0, line.Length - line.TrimStart(' ', '\t').Length);
+
+                        if (line.StartsWith(baseIndent))
+                            line = line.Substring(baseIndent.Length);
+
+                        if (line.StartsWith("#include"))
                         {
-                            //Replace the self-relative path with a path relative to $$SYS:BSP_ROOT$$.
-                            line = $"#include <{m.Groups[1]}>";
+                            var m = rgIncludeRA.Match(line);
+                            if (m.Success)
+                            {
+                                //Replace the self-relative path with a path relative to $$SYS:BSP_ROOT$$.
+                                line = $"#include <{m.Groups[1]}>";
+                            }
                         }
+
+                        //Translate the ${var.name} references to $$var.name$$
+                        var matches = rgPropertyReference.Matches(line);
+
+                        foreach (var m in matches.OfType<Match>().OrderByDescending(m => m.Index))
+                        {
+                            var vn = m.Groups[1].Value;
+                            var value = $"$${vn}$$";
+
+                            if (fixedProperties.TryGetValue(vn, out var fixedValue))
+                            {
+                                //The value was provided right here
+                                value = fixedValue;
+                            }
+                            if (propertiesByID.TryGetValue(vn, out var pe))
+                            {
+                                //This module directly defines the property referenced in this line
+                            }
+                            else if (vn.StartsWith("interface."))
+                            {
+                                //This is a special variable used to determine whether another module is referenced
+                            }
+                            else
+                            {
+                                report.ReportRawError($"{fn}: Unknown property name: {vn}");
+                            }
+
+                            line = line.Substring(0, m.Index) + value + line.Substring(m.Index + m.Length);
+
+                        }
+
+                        configLines.Add(line);
                     }
 
-                    //Translate the ${var.name} references to $$var.name$$
-                    var matches = rgPropertyReference.Matches(line);
-
-                    foreach(var m in matches.OfType<Match>().OrderByDescending(m => m.Index))
-                    {
-                        var vn = m.Groups[1].Value;
-                        var value = $"$${vn}$$";
-
-                        if (fixedProperties.TryGetValue(vn, out var fixedValue))
-                        {
-                            //The value was provided right here
-                            value = fixedValue;
-                        }
-                        if (propertiesByID.TryGetValue(vn, out var pe))
-                        {
-                            //This module directly defines the property referenced in this line
-                        }
-                        else if (vn.StartsWith("interface."))
-                        {
-                            //This is a special variable used to determine whether another module is referenced
-                        }
-                        else
-                        {
-                            report.ReportRawError($"{fn}: Unknown property name: {vn}");
-                        }
-
-                        line = line.Substring(0, m.Index) + value + line.Substring(m.Index + m.Length);
-
-                    }
-
-                    configLines.Add(line);
+                    fragments.Add(new GeneratedConfigurationFile.Fragment.BasicFragment { Lines = configLines.ToArray() });
                 }
-
-                fragments.Add(new GeneratedConfigurationFile.Fragment.BasicFragment { Lines = configLines.ToArray() });
 
                 files.Add(new GeneratedConfigurationFile
                 {
@@ -114,6 +117,24 @@ namespace renesas_ra_bsp_generator
 
             if (propertyGroups.Count > 0)
                 fw.ConfigurableProperties = new PropertyList { PropertyGroups = propertyGroups };
+        }
+
+        private bool TranslateSpecialConfigFile(XmlElement cf, List<GeneratedConfigurationFile.Fragment> fragments)
+        {
+            var id = cf.TryGetStringAttribute("id");
+            var expectedTemplate = $@"..\..\Rules\KnownTemplates\{id}.txt";
+            if (File.Exists(expectedTemplate))
+            {
+                var text = cf.SelectSingleNode("content").InnerText;
+                if (text != File.ReadAllText(expectedTemplate))
+                    throw new Exception("Unexpected special template. Please update the logic below");
+
+                var fragFile = Path.ChangeExtension(expectedTemplate, ".xml");
+                fragments.AddRange(XmlTools.LoadObject<GeneratedConfigurationFile.Fragment[]>(fragFile));
+                return true;
+            }
+
+            return false;
         }
 
         PropertyGroup TranslateModuleProperties(EmbeddedFramework fw, XmlElement cf)
@@ -201,7 +222,7 @@ namespace renesas_ra_bsp_generator
         static string ComputeCommonPrefix(IEnumerable<string> strings)
         {
             string prefix = null;
-            foreach(var str in strings)
+            foreach (var str in strings)
             {
                 if (prefix == null)
                     prefix = str;
@@ -228,27 +249,71 @@ namespace renesas_ra_bsp_generator
 
         public void TranslatePendingModuleConfigurations(BSPReportWriter report)
         {
-            foreach(var cfg in _PendingConfigurationTranslations)
+            foreach (var cfg in _PendingConfigurationTranslations)
             {
-                foreach (var prop in cfg.Xml.DocumentElement.SelectElements("raBspConfiguration/config/property"))
+                List<SysVarEntry> vars = new List<SysVarEntry>();
+                TranslateDefaultValueOverrides(report, cfg, vars);
+                TranslatePinNames(cfg, vars);
+                TranslatePinConfigurations(cfg, vars);
+
+                if (vars.Count > 0)
+                    cfg.Framework.AdditionalSystemVars = (cfg.Framework.AdditionalSystemVars ?? new SysVarEntry[0]).Concat(vars).ToArray();
+            }
+        }
+
+        private static void TranslatePinNames(PendingConfigurationTranslation cfg, List<SysVarEntry> vars)
+        {
+            List<string> pins = new List<string>();
+            Regex rgPin = new Regex("p([0-9b])([0-9]{2})\\.symbolic_name");
+
+            foreach (var prop in cfg.Xml.DocumentElement.SelectElements("raPinConfiguration/symbolicName"))
+            {
+                var id = prop.GetStringAttribute("propertyId");
+                var value = prop.GetStringAttribute("value");
+                var m = rgPin.Match(id);
+                if (!m.Success)
+                    throw new Exception("Unexpected pin name: " + id);
+
+                pins.Add(value);
+                vars.Add(new SysVarEntry { Key = $"com.renesas.ra.pin.{value}.value", Value = $"IOPORT_PORT_0{m.Groups[1]}_PIN_{m.Groups[2]}" });
+            }
+
+            vars.Add(new SysVarEntry { Key = "com.renesas.ra.device.pin_names", Value = string.Join(";", pins) });
+        }
+
+        private void TranslateDefaultValueOverrides(BSPReportWriter report, PendingConfigurationTranslation cfg, List<SysVarEntry> vars)
+        {
+            foreach (var prop in cfg.Xml.DocumentElement.SelectElements("raBspConfiguration/config/property"))
+            {
+                var id = prop.GetStringAttribute("id");
+                var value = prop.GetStringAttribute("value");
+
+                var propertyCtx = _AllProperties[id];
+                if (propertyCtx.Entry is PropertyEntry.Enumerated ep)
                 {
-                    var id = prop.GetStringAttribute("id");
-                    var value = prop.GetStringAttribute("value");
+                    string defaultValueKey = "_default." + id;
+                    var option = propertyCtx.OriginalElement.SelectSingleNode($"option[@id='{value}']") as XmlElement ?? throw new Exception("Failed to locate the option element for " + value);
 
-
-                    var propertyCtx = _AllProperties[id];
-                    if (propertyCtx.Entry is PropertyEntry.Enumerated ep)
-                    {
-                        string defaultValueKey = "_default." + id;
-                        var option = propertyCtx.OriginalElement.SelectSingleNode($"option[@id='{value}']") as XmlElement ?? throw new Exception("Failed to locate the option element for " + value);
-
-                        var optionValue = option.GetStringAttribute("value");
-                        cfg.Framework.AdditionalSystemVars = (cfg.Framework.AdditionalSystemVars ?? new SysVarEntry[0]).Concat(new[] { new SysVarEntry { Key = defaultValueKey, Value = optionValue } }).ToArray();
-                        ep.DefaultEntryValue = $"$${defaultValueKey}$$";
-                    }
-                    else
-                        report.ReportRawError($"The '{id}' property set by the {cfg.Framework.ID} framework is not an enumerated property");
+                    var optionValue = option.GetStringAttribute("value");
+                    vars.Add(new SysVarEntry { Key = defaultValueKey, Value = optionValue });
+                    ep.DefaultEntryValue = $"$${defaultValueKey}$$";
                 }
+                else
+                    report.ReportRawError($"The '{id}' property set by the {cfg.Framework.ID} framework is not an enumerated property");
+            }
+        }
+
+        private static void TranslatePinConfigurations(PendingConfigurationTranslation cfg, List<SysVarEntry> vars)
+        {
+            var pincfg = cfg.Xml.DocumentElement.SelectSingleNode("raPinConfiguration/pincfg[@active='true']") as XmlElement;
+
+            if (pincfg != null)
+            {
+                var name = pincfg.GetStringAttribute("name");
+                var symbol = pincfg.GetStringAttribute("symbol");
+
+                vars.Add(new SysVarEntry { Key = "com.renesas.ra.device.pincfg_symbols", Value = symbol });
+                vars.Add(new SysVarEntry { Key = $"com.renesas.ra.pincfg.{symbol}.name", Value = name });
             }
         }
     }
