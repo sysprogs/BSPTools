d521f6436d6701c14ab6bb6403a95e23ed6c7987
Nephtaly Aniceta <Petzep@users.noreply.github.com>
2025-12-11 09:28:57 +0100

    Update TinyEmbeddedTest.h
    Added verbosity to checks to indicate to help developers identify which tests is failing

--- TestFrameworkGenerator/Rules/TinyEmbeddedTest/framework/TinyEmbeddedTest.h
+++ TestFrameworkGenerator/Rules/TinyEmbeddedTest/framework/TinyEmbeddedTest.h
@@ -149,31 +149,32 @@ void TestInstance_ ## groupName ## _ ## testName::run()
 void RunAllTests();
 void ReportTestFailure(const char *pFormat, ...);
 
-#define CHECK(condition) ((condition) || (ReportTestFailure("Unexpected boolean value: expected true, found false"), 0))
-#define CHECK_FALSE(condition) ((!condition) || (ReportTestFailure("Unexpected boolean value: expected false, found true"), 0))
-#define CHECK_EQUAL(expected, actual) ((expected == actual) || (ReportTestFailure("Unexpected value in CHECK_EQUAL()"), 0))
+#define CHECK(condition) ((condition) || (ReportTestFailure("Unexpected boolean value in CHECK("#condition"): expected true, found false"), 0))
+#define CHECK_FALSE(condition) ((!condition) || (ReportTestFailure("Unexpected boolean value in CHECK_FALSE("#condition"): expected false, found true"), 0))
+#define CHECK_EQUAL(expected, actual) ((expected == actual) || (ReportTestFailure("Unexpected value in CHECK_EQUAL("#expected", "#actual")"), 0))
 
-#define STRCMP_EQUAL(expected, actual) ((!strcmp((expected), (actual))) || (ReportTestFailure("Unexpected string value: expected %s, found %s", (expected), (actual)), 0))
-#define STRNCMP_EQUAL(expected, actual, length) ((!strncmp((expected), (actual), (length))) || (ReportTestFailure("Unexpected string value: expected %s, found %s", (expected), (actual)), 0))
+#define STRCMP_EQUAL(expected, actual) ((!strcmp((expected), (actual))) || (ReportTestFailure("Unexpected string value in STRCMP_EQUAL("#expected", "#actual"): expected %s, found %s", (expected), (actual)), 0))
+#define STRNCMP_EQUAL(expected, actual, length) ((!strncmp((expected), (actual), (length))) || (ReportTestFailure("Unexpected string value in STRNCMP_EQUAL("#expected", "#actual", "# length"): expected %s, found %s", (expected), (actual)), 0))
 
-#define STRCMP_NOCASE_EQUAL(expected, actual) ((!strcasecmp((expected), (actual))) || (ReportTestFailure("Unexpected string value: expected %s, found %s", (expected), (actual)), 0))
-#define STRCMP_CONTAINS(expected, actual) ((strstr((actual), (expected))) || (ReportTestFailure("Unexpected string value: %s does not contain %s", (actual), (expected)), 0))
+#define STRCMP_NOCASE_EQUAL(expected, actual) ((!strcasecmp((expected), (actual))) || (ReportTestFailure("Unexpected string value in STRCMP_NOCASE_EQUAL("#expected", "#actual"): expected %s, found %s", (expected), (actual)), 0))
+#define STRCMP_CONTAINS(expected, actual) ((strstr((actual), (expected))) || (ReportTestFailure("Unexpected string value in STRCMP_CONTAINS("#expected", "#actual"): %s does not contain %s", (actual), (expected)), 0))
 
-#define LONGS_EQUAL(expected, actual) (((long)(expected) == (long)(actual)) || (ReportTestFailure("Unexpected long value: expected %ld, found %ld", (expected), (actual)), 0))
-#define UNSIGNED_LONGS_EQUAL(expected, actual) (((unsigned long)(expected) == (unsigned long)(actual)) || (ReportTestFailure("Unexpected unsigned long value: expected %uld, found %uld", (expected), (actual)), 0))
-#define UNSIGNED_LONGS_EQUAL_WITHIN(expected, actual, tolerance) (( (((unsigned long)(expected) < (unsigned long)(actual)) ? ((unsigned long)(actual) - (unsigned long)(expected)) : ((unsigned long)(expected) - (unsigned long)(actual))) <= (unsigned long)(tolerance)) || (ReportTestFailure("Unexpected unsigned long value: expected %uld, found %uld", (expected), (actual)), 0))
+#define LONGS_EQUAL(expected, actual) (((long)(expected) == (long)(actual)) || (ReportTestFailure("Unexpected long value in LONGS_EQUAL("#expected", "#actual"): expected %ld, found %ld", (expected), (actual)), 0))
+#define UNSIGNED_LONGS_EQUAL(expected, actual) (((unsigned long)(expected) == (unsigned long)(actual)) || (ReportTestFailure("Unexpected unsigned long value in UNSIGNED_LONGS_EQUAL("#expected", "#actual"): expected %uld, found %uld", (expected), (actual)), 0))
+#define UNSIGNED_LONGS_EQUAL_WITHIN(expected, actual, tolerance) (( (((unsigned long)(expected) < (unsigned long)(actual)) ? ((unsigned long)(actual) - (unsigned long)(expected)) : ((unsigned long)(expected) - (unsigned long)(actual))) <= (unsigned long)(tolerance)) || (ReportTestFailure("Unexpected unsigned long value in UNSIGNED_LONGS_EQUAL_WITHIN("#expected", "#actual", "# tolerance"): expected %uld, found %uld", (expected), (actual)), 0))
 
-#define BYTES_EQUAL(expected, actual) (((unsigned char)(expected) == (unsigned char)(actual)) || (ReportTestFailure("Unexpected byte value: expected %d, found %d", (unsigned char)(expected), (unsigned char)(actual)), 0))
-#define POINTERS_EQUAL(expected, actual) (((void *)(expected) == (void *)(actual)) || (ReportTestFailure("Unexpected pointer value: expected %p, found %p", (void *)(expected), (void *)(actual)), 0))
+#define BYTES_EQUAL(expected, actual) (((unsigned char)(expected) == (unsigned char)(actual)) || (ReportTestFailure("Unexpected byte value in BYTES_EQUAL("#expected", "#actual"): expected %d, found %d", (unsigned char)(expected), (unsigned char)(actual)), 0))
+#define POINTERS_EQUAL(expected, actual) (((void *)(expected) == (void *)(actual)) || (ReportTestFailure("Unexpected pointer value in POINTERS_EQUAL("#expected", "#actual"): expected %p, found %p", (void *)(expected), (void *)(actual)), 0))
 
-#define DOUBLES_EQUAL(expected, actual, tolerance) ((fabs((actual)-(expected)) <= (tolerance)) || (ReportTestFailure("Unexpected double value: expected %lf, found %lf", (double)expected, (double)(actual), 0), 0))
-#define DOUBLES_NOT_EQUAL(expected, actual, tolerance) ((fabs((double)(actual)-(double)(expected)) > (double)(tolerance)) || (ReportTestFailure("Unexpected double value: expected not equal to %f, found %f", (double)(expected), (double)(actual)), 0))
+#define DOUBLES_EQUAL(expected, actual, tolerance) ((fabs((actual)-(expected)) <= (tolerance)) || (ReportTestFailure("Unexpected double value in DOUBLES_EQUAL("#expected", "#actual", "# tolerance"): expected %lf, found %lf", (double)expected, (double)(actual), 0), 0))
+#define DOUBLES_NOT_EQUAL(expected, actual, tolerance) ((fabs((double)(actual)-(double)(expected)) > (double)(tolerance)) || (ReportTestFailure("Unexpected double value in DOUBLES_NOT_EQUAL("#expected", "#actual", "# tolerance"): expected not equal to %f, found %f", (double)(expected), (double)(actual)), 0))
 #define MEMCMP_EQUAL(expected, actual, size) ((!memcmp((expected), (actual), (size))) || (ReportTestFailure("Unexpected memory block contents"), 0))
 
-#define FLOATS_EQUAL(expected, actual, tolerance) ((fabsf((float)(actual)-(float)(expected)) <= (float)(tolerance)) || (ReportTestFailure("Unexpected float value: expected %f, found %f", (float)(expected), (float)(actual)), 0))
-#define FLOATS_NOT_EQUAL(expected, actual, tolerance) ((fabsf((float)(actual)-(float)(expected)) > (float)(tolerance)) || (ReportTestFailure("Unexpected float value: expected not equal to %f, found %f", (float)(expected), (float)(actual)), 0))
+#define FLOATS_EQUAL(expected, actual, tolerance) ((fabsf((float)(actual)-(float)(expected)) <= (float)(tolerance)) || (ReportTestFailure("Unexpected float value in FLOATS_EQUAL("#expected", "#actual", "# tolerance"): expected %f, found %f", (float)(expected), (float)(actual)), 0))
+#define FLOATS_NOT_EQUAL(expected, actual, tolerance) ((fabsf((float)(actual)-(float)(expected)) > (float)(tolerance)) || (ReportTestFailure("Unexpected float value in FLOATS_NOT_EQUAL("#expected", "#actual", "# tolerance"): expected not equal to %f, found %f", (float)(expected), (float)(actual)), 0))
 
-#define BITS_EQUAL(expected, actual, mask) ((((expected) & (mask)) == ((actual) & (mask))) || (ReportTestFailure("Unexpected value: expected %x, found %x", (expected) & (mask), (actual) & (mask)), 0))
+#define BITS_EQUAL(expected, actual, mask) ((((expected) & (mask)) == ((actual) & (mask))) || (ReportTestFailure("Unexpected value in BITS_EQUAL("#expected", "#actual", "# mask"): expected %x, found %x", (expected) & (mask), (actual) & (mask)), 0))
 #define FAIL(text) ReportTestFailure(text)
 
 void OutputTestMessage(const char *pMessage);
+
